5cf49facdaba5c1b3c0f04b7a1a395f994d974cc
Kaiki de Almeida Fischer <145053286+kaikifischer@users.noreply.github.com>
2025-05-12 21:43:36 -0300

    Add files via upload

---
+++ src/main/java/br/cefsa/edu/br/bootcampduo/controller/BootcampduoController.java
@@ -0,0 +1,56 @@
+package br.cefsa.edu.br.bootcampduo.controller;
+        
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bootcamp-duo")
+public class BootcampduoController {
+    @GetMapping("")
+    public String index(){
+        return "/index";
+    }
+
+    @GetMapping("/games")
+    public String games(){
+        return "/bootcamp/games";
+    }
+
+    @GetMapping("/mobile/android")
+    public String mobileAndroid() {
+        return "/bootcamp/mobile-android";
+    }
+
+    @GetMapping("/mobile/ios")
+    public String mobileIos() {
+        return "/bootcamp/mobile-ios";
+    }
+
+    @GetMapping("/devops")
+    public String devops() {
+        return "/bootcamp/devops";
+    }
+    
+    @GetMapping("/website/html")
+    public String websiteHtml() {
+        return "/bootcamp/website-html";
+    }
+
+    @GetMapping("/website/css")
+    public String websiteCss() {
+        return "/bootcamp/website-css";
+    }
+    
+    @GetMapping("/website/javascript")
+    public String websiteJavascript() {
+        return "/bootcamp/website-javascript";
+    }
+
+    @GetMapping("/inteligencia-artificial")
+    public String inteligenciaArtificial() {
+        return "/bootcamp/inteligencia-artificial";
+    }
+}
+
+
---
+++ src/test/java/br/edu/fesa/bootcampduo/BootcampduoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fesa.bootcampduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
