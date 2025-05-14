804aabf85917a5957b5a0940a44a44c27407ec3f
Kaiki Fischer <kaiki.fischer@hotmail.com>
2025-05-14 20:12:26 -0300

    error fix

---
+++ src/main/java/br/edu/fesa/bootcampduo/BootcampduoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fesa.bootcampduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampduoApplication.class, args);
+	}
+
+}
